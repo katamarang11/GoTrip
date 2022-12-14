5d98f09ddc86ad75cbada1d82248030b5b9d9175
katamarang11 <102056956+katamarang11@users.noreply.github.com>
2022-12-14 23:44:14 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// Carusel
+const owl = $('.owl-carousel');
+owl.owlCarousel({
+    center: true,
+    loop: true,
+    margin: 20,
+    startPosition: 0,
+    items: 1,
+    responsive: {
+        540: {
+            items: 3,
+            startPosition: 1,
+        },
+        800: {
+            items: 3,
+            startPosition: 1,
+        },
+        850: {
+            items: 3,
+            startPosition : 1,
+        },
+        1000: {
+            items: 3,
+        },
+        1200: {
+            margin: 30,
+            items: 3,
+        }
+    }
+});
+
+
+$('.slider__btn--prev').click(function() {
+    owl.trigger('prev.owl.carousel');
+});
+
+$('.slider__btn--next').click(function() {
+    owl.trigger('next.owl.carousel');
+});
+
+
+// Nav icon
+const navBtn = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav');
+const menuIcon = document.querySelector('.menu-icon');
+
+navBtn.onclick = function () {
+    nav.classList.toggle('nav--mobile');
+    menuIcon.classList.toggle('menu-icon--active');
+    document.body.classList.toggle('no-scroll');
+};
